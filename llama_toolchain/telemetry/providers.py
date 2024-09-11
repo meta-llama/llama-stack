191cd28831109a37e2ffc99bd63547b6ad75724f
Ashwin Bharambe <ashwin.bharambe@gmail.com>
2024-09-11 14:25:37 -0700

    Simplified Telemetry API and tying it to logger (#57)
    * Simplified Telemetry API and tying it to logger
    
    * small update which adds a METRIC type
    
    * move span events one level down into structured log events
    
    ---------
    
    Co-authored-by: Ashwin Bharambe <ashwin@meta.com>

---
+++ llama_toolchain/telemetry/providers.py
@@ -0,0 +1,21 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the terms described in the LICENSE file in
+# the root directory of this source tree.
+
+from typing import List
+
+from llama_toolchain.core.datatypes import *  # noqa: F403
+
+
+def available_providers() -> List[ProviderSpec]:
+    return [
+        InlineProviderSpec(
+            api=Api.telemetry,
+            provider_type="console",
+            pip_packages=[],
+            module="llama_toolchain.telemetry.console",
+            config_class="llama_toolchain.telemetry.console.ConsoleConfig",
+        ),
+    ]
