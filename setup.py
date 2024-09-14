7a283ea07639cd70c1bd3d1327ce1c649fd38a9d
Ashwin Bharambe <ashwin.bharambe@gmail.com>
2024-09-13 17:23:12 -0700

    Bump version to 0.0.15

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def read_requirements():
 
 setup(
     name="llama_toolchain",
-    version="0.0.14",
+    version="0.0.15",
     author="Meta Llama",
     author_email="llama-oss@meta.com",
     description="Llama toolchain",
