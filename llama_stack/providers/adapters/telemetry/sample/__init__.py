e4324f488e9785ce33d905dfce6308b2022dbee1
Ashwin Bharambe <ashwin.bharambe@gmail.com>
2024-09-23 14:13:46 -0700

    fix sample

--- llama_stack/providers/adapters/telemetry/sample/__init__.py
+++ llama_stack/providers/adapters/telemetry/sample/__init__.py
@@ -4,6 +4,8 @@
 # This source code is licensed under the terms described in the LICENSE file in
 # the root directory of this source tree.
 
+from typing import Any
+
 from .config import SampleConfig
 
 
