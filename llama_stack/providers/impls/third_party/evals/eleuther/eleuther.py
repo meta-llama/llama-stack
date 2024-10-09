0919072a3334903956d760f8f299018d67a4749d
Xi Yan <xiyan@meta.com>
2024-10-08 23:22:50 -0700

    eleuther custom tasks

--- llama_stack/providers/impls/third_party/evals/eleuther/eleuther.py
+++ llama_stack/providers/impls/third_party/evals/eleuther/eleuther.py
@@ -6,12 +6,15 @@
 
 from llama_stack.apis.inference import *  # noqa: F403
 from llama_stack.apis.evals import *  # noqa: F403
+import os
 import random
+from pathlib import Path
 
 import lm_eval
 from lm_eval.api.model import LM
 from lm_eval.evaluator import evaluate, get_task_list
 from lm_eval.tasks import get_task_dict, TaskManager
+from termcolor import cprint
 
 from .config import EleutherEvalsImplConfig  # noqa
 
@@ -20,10 +23,12 @@ class EleutherEvalsWrapper(LM):
     def __init__(
         self,
         inference_api: Inference,
+        model: str,
         **kwargs,
     ):
         super().__init__(**kwargs)
         self.inference_api = inference_api
+        self.model = model
         self.tokenizer = None
         self.tokenized_requests = False
         self.kwargs = kwargs
@@ -83,13 +88,29 @@ class EleutherEvalsWrapper(LM):
         raise NotImplementedError("No support for logits.")
 
     def generate_until(self, requests, disable_tqdm: bool = False) -> List[str]:
-        return NotImplementedError("Not implemented")
+        res = []
+        for req in requests:
+            print("generation for msg: ", req.args[0])
+            response = self.inference_api.chat_completion(
+                model=self.model,
+                messages=[
+                    {
+                        "role": "user",
+                        "content": req.args[0],
+                    }
+                ],
+                stream=False,
+            )
+            print(response)
+            res.append(response.completion_message)
+
+        print(response)
+        return res
 
 
 class EleutherEvalsAdapter(Evals):
     def __init__(self, config: EleutherEvalsImplConfig, inference_api: Inference):
         self.inference_api = inference_api
-        self.eluther_wrapper = EleutherEvalsWrapper(inference_api)
 
     async def initialize(self) -> None:
         pass
@@ -103,16 +124,34 @@ class EleutherEvalsAdapter(Evals):
         dataset: str,
         task: str,
     ) -> EvaluateResponse:
-        task_manager = TaskManager()
+        eluther_wrapper = EleutherEvalsWrapper(self.inference_api, model)
+
+        cprint(f"Eleuther Evals: {model} {dataset} {task}", "red")
+
+        task = "meta_mmlu_pro_instruct"
+        current_dir = Path(os.path.dirname(os.path.abspath(__file__)))
+        print(current_dir)
+
+        task_manager = TaskManager(
+            include_path=str(current_dir / "tasks"),
+        )
+
         task_dict = get_task_dict(task, task_manager)
+        cprint(task_dict, "blue")
+
         task_types = set([t.task.OUTPUT_TYPE for t in get_task_list(task_dict)])
+        cprint(task_types, "cyan")
 
         output = evaluate(
-            self.eluther_wrapper,
+            eluther_wrapper,
             task_dict,
-            limit=2,
+            limit=1,
         )
+
         formatted_output = lm_eval.utils.make_table(output)
+
+        cprint(formatted_output, "green")
+
         return EvaluateResponse(
             metrics={
                 "metrics_table": formatted_output,
