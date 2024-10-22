e709101f5b9f2731f7083ce2cb76f8dd327c172c
Xi Yan <xiyan@meta.com>
2024-10-22 08:24:14 -0700

    datasets api

---
+++ llama_stack/apis/datasets/datasets.py
@@ -0,0 +1,65 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the terms described in the LICENSE file in
+# the root directory of this source tree.
+
+from typing import Any, Dict, Optional, Protocol
+
+from llama_models.llama3.api.datatypes import URL
+
+from llama_models.schema_utils import json_schema_type, webmethod
+
+from pydantic import BaseModel
+
+from llama_stack.apis.common.type_system import ParamType
+
+
+@json_schema_type
+class DatasetSchema(BaseModel):
+    columns: Dict[str, ParamType]
+
+
+@json_schema_type
+class DatasetDef(BaseModel):
+    identifier: str = Field(
+        description="A unique name for the dataset",
+    )
+    schema: DatasetSchema = Field(
+        description="The schema definition for this dataset",
+    )
+    url: URL
+    metadata: Dict[str, Any] = Field(
+        default_factory=dict,
+        description="Any additional metadata for this dataset",
+    )
+
+
+@json_schema_type
+class DatasetDefWithProvider(DatasetDef):
+    provider_id: str = Field(
+        description="ID of the provider which serves this dataset",
+    )
+
+
+class Datasets(Protocol):
+    @webmethod(route="/datasets/register", method="POST")
+    async def register_dataset(
+        self,
+        dataset_def: DatasetDefWithProvider,
+    ) -> None: ...
+
+    @webmethod(route="/datasets/get", method="GET")
+    async def get_dataset(
+        self,
+        dataset_identifier: str,
+    ) -> Optional[DatasetDefWithProvider]: ...
+
+    @webmethod(route="/datasets/delete")
+    async def delete_dataset(
+        self,
+        dataset_identifier: str,
+    ) -> None: ...
+
+    @webmethod(route="/datasets/list", method="GET")
+    async def list_datasets(self) -> List[DatasetDefWithProvider]: ...
