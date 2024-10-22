68046e25a1c9200dd049f1299a1459d1ede9b6ad
Raghu Murthy <rmurthy@fb.com>
2024-10-22 05:53:33 +0530

    fixing pre-commit issues

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,9 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the terms described in the LICENSE file in
+# the root directory of this source tree.
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -6,17 +12,17 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'llama-stack'
-copyright = '2024, Meta'
-author = 'Meta'
+project = "llama-stack"
+copyright = "2024, Meta"
+author = "Meta"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['myst_parser']
+extensions = ["myst_parser"]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 myst_enable_extensions = [
     "amsmath",
@@ -38,10 +44,10 @@ myst_enable_extensions = [
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = "alabaster"
 html_theme_options = {
     "canonical_url": "https://github.com/meta-llama/llama-stack",
 }
 
-html_static_path = ['../_static']
-html_logo = '../_static/llama-stack-logo.png'
+html_static_path = ["../_static"]
+html_logo = "../_static/llama-stack-logo.png"
