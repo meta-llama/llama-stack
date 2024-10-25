e83ce367800e1491236a1f67ae89db042ca49c90
Dinesh Yeduguru <dineshyv@meta.com>
2024-10-25 11:03:05 -0700

    fix precommit hook ci failure

--- llama_stack/providers/impls/meta_reference/inference/quantization/scripts/build_conda.sh
+++ llama_stack/providers/impls/meta_reference/inference/quantization/scripts/build_conda.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the terms described in the LICENSE file in
+# the root directory of this source tree.
+
 if [[ $# -ne 1 ]]; then
     echo "Error: Please provide the name of CONDA environment you wish to create"
     exit 1
