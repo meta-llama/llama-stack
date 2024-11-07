31c5fbda5e1484e5d3b241d8b60ae689530c9a7c
Yufei (Benny) Chen <1585539+benjibc@users.noreply.github.com>
2024-11-07 10:11:28 -0800

    [LlamaStack][Fireworks] Update client and add unittest (#390)

--- llama_stack/providers/remote/inference/fireworks/config.py
+++ llama_stack/providers/remote/inference/fireworks/config.py
@@ -4,6 +4,8 @@
 # This source code is licensed under the terms described in the LICENSE file in
 # the root directory of this source tree.
 
+from typing import Optional
+
 from llama_models.schema_utils import json_schema_type
 from pydantic import BaseModel, Field
 
@@ -14,7 +16,7 @@ class FireworksImplConfig(BaseModel):
         default="https://api.fireworks.ai/inference",
         description="The URL for the Fireworks server",
     )
-    api_key: str = Field(
-        default="",
+    api_key: Optional[str] = Field(
+        default=None,
         description="The Fireworks.ai API Key",
     )
