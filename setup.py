f6aaa9c70886729f56f4626fc84079de94cbf803
Xi Yan <xiyan@meta.com>
2024-11-08 17:28:39 -0800

    Bump version to 0.0.50

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def read_requirements():
 
 setup(
     name="llama_stack",
-    version="0.0.49",
+    version="0.0.50",
     author="Meta Llama",
     author_email="llama-oss@meta.com",
     description="Llama Stack",
