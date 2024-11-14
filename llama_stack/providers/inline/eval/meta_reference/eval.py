2080cc89fa0d2662a5d7252f5e1deba86324f737
Xi Yan <xiyan@meta.com>
2024-11-14 00:05:24 -0500

    remove prints

--- llama_stack/providers/inline/eval/meta_reference/eval.py
+++ llama_stack/providers/inline/eval/meta_reference/eval.py
@@ -53,7 +53,7 @@ class MetaReferenceEvalImpl(Eval, EvalTasksProtocolPrivate):
 
     async def initialize(self) -> None:
         self.kvstore = await kvstore_impl(self.config.kvstore)
-        # Load existing datasets from kvstore
+        # Load existing eval_tasks from kvstore
         start_key = EVAL_TASKS_PREFIX
         end_key = f"{EVAL_TASKS_PREFIX}\xff"
         stored_eval_tasks = await self.kvstore.range(start_key, end_key)
@@ -62,19 +62,15 @@ class MetaReferenceEvalImpl(Eval, EvalTasksProtocolPrivate):
             eval_task = EvalTask.model_validate_json(eval_task)
             self.eval_tasks[eval_task.identifier] = eval_task
 
-        print(f"Meta Reference Eval initialized with {self.eval_tasks}")
-
     async def shutdown(self) -> None: ...
 
     async def register_eval_task(self, task_def: EvalTask) -> None:
-        print("REGISTER EVAL TASK")
         # Store in kvstore
         key = f"{EVAL_TASKS_PREFIX}{task_def.identifier}"
         await self.kvstore.set(
             key=key,
             value=task_def.json(),
         )
-        print(self.eval_tasks)
         self.eval_tasks[task_def.identifier] = task_def
 
     async def validate_eval_input_dataset_schema(self, dataset_id: str) -> None:
