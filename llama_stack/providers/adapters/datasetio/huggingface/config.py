04ff184b34a09ad260396dd118da85ce7d1b51f6
Xi Yan <xiyan@meta.com>
2024-11-13 23:44:48 -0500

    persist local hf datasetio

--- llama_stack/providers/adapters/datasetio/huggingface/config.py
+++ llama_stack/providers/adapters/datasetio/huggingface/config.py
@@ -3,7 +3,15 @@
 #
 # This source code is licensed under the terms described in the LICENSE file in
 # the root directory of this source tree.
-from llama_stack.apis.datasetio import *  # noqa: F401, F403
+from llama_stack.distribution.utils.config_dirs import RUNTIME_BASE_DIR
+from llama_stack.providers.utils.kvstore.config import (
+    KVStoreConfig,
+    SqliteKVStoreConfig,
+)
+from pydantic import BaseModel
 
 
-class HuggingfaceDatasetIOConfig(BaseModel): ...
+class HuggingfaceDatasetIOConfig(BaseModel):
+    kvstore: KVStoreConfig = SqliteKVStoreConfig(
+        db_path=(RUNTIME_BASE_DIR / "huggingface_datasetio.db").as_posix()
+    )  # Uses SQLite config specific to FAISS storage
