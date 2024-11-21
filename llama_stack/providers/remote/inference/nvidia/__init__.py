988741c27698c907c871126d9a6a1f32a8aeb393
Matthew Farrellee <matt@cs.wisc.edu>
2024-11-21 13:15:31 -0500

    dynamically import NVIDIAInferenceAdapter

--- llama_stack/providers/remote/inference/nvidia/__init__.py
+++ llama_stack/providers/remote/inference/nvidia/__init__.py
@@ -4,11 +4,15 @@
 # This source code is licensed under the terms described in the LICENSE file in
 # the root directory of this source tree.
 
+from llama_stack.apis.inference import Inference
+
 from ._config import NVIDIAConfig
-from ._nvidia import NVIDIAInferenceAdapter
 
 
-async def get_adapter_impl(config: NVIDIAConfig, _deps) -> NVIDIAInferenceAdapter:
+async def get_adapter_impl(config: NVIDIAConfig, _deps) -> Inference:
+    # import dynamically so `llama stack build` does not fail due to missing dependencies
+    from ._nvidia import NVIDIAInferenceAdapter
+
     if not isinstance(config, NVIDIAConfig):
         raise RuntimeError(f"Unexpected config type: {type(config)}")
     adapter = NVIDIAInferenceAdapter(config)
