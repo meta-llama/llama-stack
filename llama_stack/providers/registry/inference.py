a7df3e539cb131d867c6b1919862a532696d0e86
seyeong-han <h960213@gmail.com>
2024-11-23 17:55:04 -0600

    feat: add ssambanova provider

--- llama_stack/providers/registry/inference.py
+++ llama_stack/providers/registry/inference.py
@@ -150,4 +150,15 @@ def available_providers() -> List[ProviderSpec]:
                 config_class="llama_stack.providers.remote.inference.databricks.DatabricksImplConfig",
             ),
         ),
+        remote_provider_spec(
+            api=Api.inference,
+            adapter=AdapterSpec(
+                adapter_type="ssambanova",
+                pip_packages=[
+                    "openai",
+                ],
+                module="llama_stack.providers.remote.inference.ssambanova",
+                config_class="llama_stack.providers.remote.inference.ssambanova.SsambanovaImplConfig",
+            ),
+        ),
     ]
