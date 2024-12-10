02b43be9d78b7a3967c0800d507434f9d04339ba
Ashwin Bharambe <ashwin.bharambe@gmail.com>
2024-12-10 10:18:44 -0800

    Bump version to 0.0.61

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def read_requirements():
 
 setup(
     name="llama_stack",
-    version="0.0.60",
+    version="0.0.61",
     author="Meta Llama",
     author_email="llama-oss@meta.com",
     description="Llama Stack",
