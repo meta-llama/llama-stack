a4bcfb8bbaae13a78030ea2ac8c68b155091d65f
Xi Yan <xiyan@meta.com>
2024-12-11 10:03:42 -0800

    [/scoring] add ability to define aggregation functions for scoring functions & refactors (#597)
    # What does this PR do?
    
    - Add ability to define aggregation functions for scoring functions via
    `ScoringFnParams`
    - Supported by `basic` / `regex_parser` / `llm_as_judge` scoring
    functions
    
    
    ## Test Plan
    
    ```
    pytest -v -s -m basic_scoring_together_inference scoring/test_scoring.py
    ```
    <img width="855" alt="image"
    src="https://github.com/user-attachments/assets/12db8e6e-2ad4-462e-b9b9-70ba6c050a6c">
    
    
    ```
    pytest -v -s -m llm_as_judge_scoring_together_inference scoring/test_scoring.py
    ```
    <img width="858" alt="image"
    src="https://github.com/user-attachments/assets/bf806676-6f5e-456d-be9f-f81a26d1df19">
    
    
    
    **Example Response** (`basic`)
    <img width="863" alt="image"
    src="https://github.com/user-attachments/assets/0e57a49c-8386-45cc-8fa9-3e61aaa9a3be">
    
    **Example Response** (`llm-as-judge`)
    <img width="854" alt="image"
    src="https://github.com/user-attachments/assets/38065bc2-b724-47ed-9535-79b6099c4362">
    
    
    ## Sources
    
    Please link relevant resources if necessary.
    
    
    ## Before submitting
    
    - [ ] This PR fixes a typo or improves the docs (you can dismiss the
    other checks if that's the case).
    - [ ] Ran pre-commit to handle lint / formatting issues.
    - [ ] Read the [contributor
    guideline](https://github.com/meta-llama/llama-stack/blob/main/CONTRIBUTING.md),
          Pull Request section?
    - [ ] Updated relevant documentation.
    - [ ] Wrote necessary unit or integration tests.

--- llama_stack/providers/tests/scoring/test_scoring.py
+++ llama_stack/providers/tests/scoring/test_scoring.py
@@ -7,7 +7,12 @@
 
 import pytest
 
-from llama_stack.apis.scoring_functions import *  # noqa: F403
+from llama_stack.apis.scoring_functions import (
+    AggregationFunctionType,
+    BasicScoringFnParams,
+    LLMAsJudgeScoringFnParams,
+    RegexParserScoringFnParams,
+)
 from llama_stack.distribution.datatypes import Api
 from llama_stack.providers.tests.datasetio.test_datasetio import register_dataset
 
@@ -16,6 +21,11 @@ from llama_stack.providers.tests.datasetio.test_datasetio import register_datase
 # pytest llama_stack/providers/tests/scoring/test_scoring.py
 #   -m "meta_reference"
 #   -v -s --tb=short --disable-warnings
+
+
+@pytest.fixture
+def sample_judge_prompt_template():
+    return "Output a number response in the following format: Score: <number>, where <number> is the number between 0 and 9."
 
 
 class TestScoring:
@@ -92,7 +102,9 @@ class TestScoring:
             assert len(response.results[x].score_rows) == 5
 
     @pytest.mark.asyncio
-    async def test_scoring_score_with_params(self, scoring_stack):
+    async def test_scoring_score_with_params_llm_as_judge(
+        self, scoring_stack, sample_judge_prompt_template
+    ):
         (
             scoring_impl,
             scoring_functions_impl,
@@ -129,10 +141,11 @@ class TestScoring:
         assert len(rows.rows) == 3
 
         scoring_functions = {
-            "llm-as-judge::llm_as_judge_base": LLMAsJudgeScoringFnParams(
+            "llm-as-judge::base": LLMAsJudgeScoringFnParams(
                 judge_model="Llama3.1-405B-Instruct",
-                prompt_template="Output a number response in the following format: Score: <number>, where <number> is the number between 0 and 9.",
+                prompt_template=sample_judge_prompt_template,
                 judge_score_regexes=[r"Score: (\d+)"],
+                aggregation_functions=[AggregationFunctionType.categorical_count],
             )
         }
 
@@ -154,3 +167,67 @@ class TestScoring:
         for x in scoring_functions:
             assert x in response.results
             assert len(response.results[x].score_rows) == 5
+
+    @pytest.mark.asyncio
+    async def test_scoring_score_with_aggregation_functions(
+        self, scoring_stack, sample_judge_prompt_template
+    ):
+        (
+            scoring_impl,
+            scoring_functions_impl,
+            datasetio_impl,
+            datasets_impl,
+            models_impl,
+        ) = (
+            scoring_stack[Api.scoring],
+            scoring_stack[Api.scoring_functions],
+            scoring_stack[Api.datasetio],
+            scoring_stack[Api.datasets],
+            scoring_stack[Api.models],
+        )
+        await register_dataset(datasets_impl)
+        rows = await datasetio_impl.get_rows_paginated(
+            dataset_id="test_dataset",
+            rows_in_page=3,
+        )
+        assert len(rows.rows) == 3
+
+        scoring_fns_list = await scoring_functions_impl.list_scoring_functions()
+        scoring_functions = {}
+        aggr_fns = [
+            AggregationFunctionType.accuracy,
+            AggregationFunctionType.median,
+            AggregationFunctionType.categorical_count,
+            AggregationFunctionType.average,
+        ]
+        for x in scoring_fns_list:
+            if x.provider_id == "llm-as-judge":
+                aggr_fns = [AggregationFunctionType.categorical_count]
+                scoring_functions[x.identifier] = LLMAsJudgeScoringFnParams(
+                    judge_model="Llama3.1-405B-Instruct",
+                    prompt_template=sample_judge_prompt_template,
+                    judge_score_regexes=[r"Score: (\d+)"],
+                    aggregation_functions=aggr_fns,
+                )
+            elif x.provider_id == "basic":
+                if "regex_parser" in x.identifier:
+                    scoring_functions[x.identifier] = RegexParserScoringFnParams(
+                        aggregation_functions=aggr_fns,
+                    )
+                else:
+                    scoring_functions[x.identifier] = BasicScoringFnParams(
+                        aggregation_functions=aggr_fns,
+                    )
+            else:
+                scoring_functions[x.identifier] = None
+
+        response = await scoring_impl.score(
+            input_rows=rows.rows,
+            scoring_functions=scoring_functions,
+        )
+
+        assert len(response.results) == len(scoring_functions)
+        for x in scoring_functions:
+            assert x in response.results
+            assert len(response.results[x].score_rows) == len(rows.rows)
+            assert len(response.results[x].aggregated_results) == len(aggr_fns)
