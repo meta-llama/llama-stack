51457b0750329b38c14be5c1451a86d88487d7f6
Xi Yan <xiyan@meta.com>
2024-12-30 14:10:14 -0800

    tmp debug meta-ref

--- llama_stack/providers/tests/resolver.py
+++ llama_stack/providers/tests/resolver.py
@@ -58,6 +58,7 @@ async def construct_stack_for_test(
         scoring_fns=scoring_fns or [],
         eval_tasks=eval_tasks or [],
     )
+    cprint(f"run_config BEFORE: {run_config}", "yellow")
     run_config = parse_and_maybe_upgrade_config(run_config)
     cprint(f"run_config AFTER: {run_config}", "red")
     try:
