518c5ed135c51ed0791d136ce23ad4df94608ad4
Xi Yan <xiyan@meta.com>
2024-12-30 13:41:36 -0800

    tmp debug meta-ref

--- llama_stack/distribution/configure.py
+++ llama_stack/distribution/configure.py
@@ -176,6 +176,7 @@ def upgrade_from_routing_table(
     config_dict["apis"] = config_dict["apis_to_serve"]
     config_dict.pop("apis_to_serve", None)
 
+    print(config_dict)
     return config_dict
 
 
