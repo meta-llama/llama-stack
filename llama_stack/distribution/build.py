8c342e18769a19e7aa2c88cf8e3525baf9cdbc5e
Xi Yan <xiyan@meta.com>
2025-01-17 22:21:13 -0800

    remove print

--- llama_stack/distribution/build.py
+++ llama_stack/distribution/build.py
@@ -130,7 +130,6 @@ def build_image(
             str(BUILDS_BASE_DIR / ImageType.container.value),
             " ".join(normal_deps),
         ]
-        print("!!!! args", args)
     elif build_config.image_type == ImageType.conda.value:
         script = str(
             importlib.resources.files("llama_stack") / "distribution/build_conda_env.sh"
