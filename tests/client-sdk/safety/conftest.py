72a1b27d01d7c7ec644a85f42d06b9df7dadc913
Ashwin Bharambe <ashwin.bharambe@gmail.com>
2025-01-22 18:09:46 -0800

    nitpick

--- tests/client-sdk/safety/conftest.py
+++ tests/client-sdk/safety/conftest.py
@@ -7,7 +7,7 @@
 
 def pytest_addoption(parser):
     parser.addoption(
-        "--safety_shield",
+        "--safety-shield",
         action="store",
         default="meta-llama/Llama-Guard-3-1B",
         help="Specify the safety shield model to use for testing",
@@ -18,5 +18,5 @@ def pytest_generate_tests(metafunc):
     if "llama_guard_text_shield_id" in metafunc.fixturenames:
         metafunc.parametrize(
             "llama_guard_text_shield_id",
-            [metafunc.config.getoption("--safety_shield")],
+            [metafunc.config.getoption("--safety-shield")],
         )
