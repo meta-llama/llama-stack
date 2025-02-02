4773092dd18aab8df1145f8a9f424254a1791567
Yuan Tang <terrytangyuan@gmail.com>
2025-02-01 22:14:29 -0500

    Fix UBI9 image build when installing Python packages via uv (#926)
    This was missed in https://github.com/meta-llama/llama-stack/pull/921. 
    
    cc @ashwinb @hardikjshah
    
    Signed-off-by: Yuan Tang <terrytangyuan@gmail.com>

--- llama_stack/distribution/build_container.sh
+++ llama_stack/distribution/build_container.sh
@@ -63,6 +63,8 @@ RUN microdnf -y update && microdnf install -y iputils net-tools wget \
     vim-minimal python3.11 python3.11-pip python3.11-wheel \
     python3.11-setuptools && ln -s /bin/pip3.11 /bin/pip && ln -s /bin/python3.11 /bin/python && microdnf clean all
 
+ENV UV_SYSTEM_PYTHON=1
+RUN pip install uv
 EOF
 else
   add_to_container << EOF
