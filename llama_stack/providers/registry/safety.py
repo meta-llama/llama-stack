358db6ebc39889da115f9003bf31f3b4a2d305f7
Chantal D Gama Rose <cdgamarose@nvidia.com>
2025-02-19 16:36:56 -0800

    adding nvidia to registry

--- llama_stack/providers/registry/safety.py
+++ llama_stack/providers/registry/safety.py
@@ -85,4 +85,13 @@ Provider `inline::meta-reference` for API `safety` does not work with the latest
                 config_class="llama_stack.providers.remote.safety.bedrock.BedrockSafetyConfig",
             ),
         ),
+        remote_provider_spec(
+            api=Api.safety,
+            adapter=AdapterSpec(
+                adapter_type="nvidia",
+                pip_packages=[],
+                module="llama_stack.providers.remote.safety.nvidia",
+                config_class="llama_stack.providers.remote.safety.nvida.NVIDIASafetyConfig",
+            ),
+        ),
     ]
