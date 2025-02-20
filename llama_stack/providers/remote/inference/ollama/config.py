9ca8bc4ba286f0f328f1fa529ab2f9626346cec9
Haiping Zhao <hzhao@meta.com>
2025-02-20 11:23:31 -0800

    "fbcode c3520fd390a77507cbf4d44393586a9286dd6efa(D69478008)"

--- llama_stack/providers/remote/inference/ollama/config.py
+++ llama_stack/providers/remote/inference/ollama/config.py
@@ -4,6 +4,7 @@
 # This source code is licensed under the terms described in the LICENSE file in
 # the root directory of this source tree.
 
+import os
 from typing import Any, Dict
 
 from pydantic import BaseModel
@@ -12,7 +13,7 @@ DEFAULT_OLLAMA_URL = "http://localhost:11434"
 
 
 class OllamaImplConfig(BaseModel):
-    url: str = DEFAULT_OLLAMA_URL
+    url: str = os.getenv("OLLAMA_URL", DEFAULT_OLLAMA_URL)
 
     @classmethod
     def sample_run_config(cls, url: str = "${env.OLLAMA_URL:http://localhost:11434}", **kwargs) -> Dict[str, Any]:
