ec52c46ff28320733a678cb77b0150269aa77f42
Ashwin Bharambe <ashwin.bharambe@gmail.com>
2025-02-24 14:03:54 -0800

    tests: add a ci-tests distro template for running e2e tests

--- llama_stack/templates/ci-tests/ci_tests.py
+++ llama_stack/templates/ci-tests/ci_tests.py
@@ -4,7 +4,6 @@
 # This source code is licensed under the terms described in the LICENSE file in
 # the root directory of this source tree.
 
-from pathlib import Path
 
 from llama_stack.apis.models.models import ModelType
 from llama_stack.distribution.datatypes import (
@@ -17,7 +16,7 @@ from llama_stack.models.llama.sku_list import all_registered_models
 from llama_stack.providers.inline.inference.sentence_transformers import (
     SentenceTransformersInferenceConfig,
 )
-from llama_stack.providers.inline.vector_io.faiss.config import FaissVectorIOConfig
+from llama_stack.providers.inline.vector_io.sqlite_vec.config import SQLiteVectorIOConfig
 from llama_stack.providers.remote.inference.fireworks.config import FireworksImplConfig
 from llama_stack.providers.remote.inference.fireworks.models import MODEL_ENTRIES
 from llama_stack.templates.template import DistributionTemplate, RunConfigSettings
@@ -26,7 +25,7 @@ from llama_stack.templates.template import DistributionTemplate, RunConfigSettin
 def get_distribution_template() -> DistributionTemplate:
     providers = {
         "inference": ["remote::fireworks", "inline::sentence-transformers"],
-        "vector_io": ["inline::faiss", "remote::chromadb", "remote::pgvector"],
+        "vector_io": ["inline::sqlite-vec", "remote::chromadb", "remote::pgvector"],
         "safety": ["inline::llama-guard"],
         "agents": ["inline::meta-reference"],
         "telemetry": ["inline::meta-reference"],
@@ -41,23 +40,21 @@ def get_distribution_template() -> DistributionTemplate:
             "remote::model-context-protocol",
         ],
     }
-
-    name = "fireworks"
-
+    name = "ci-tests"
     inference_provider = Provider(
         provider_id="fireworks",
         provider_type="remote::fireworks",
         config=FireworksImplConfig.sample_run_config(),
     )
+    vector_io_provider = Provider(
+        provider_id="sqlite-vec",
+        provider_type="inline::sqlite-vec",
+        config=SQLiteVectorIOConfig.sample_run_config(f"distributions/{name}"),
+    )
     embedding_provider = Provider(
         provider_id="sentence-transformers",
         provider_type="inline::sentence-transformers",
         config=SentenceTransformersInferenceConfig.sample_run_config(),
-    )
-    vector_io_provider = Provider(
-        provider_id="faiss",
-        provider_type="inline::faiss",
-        config=FaissVectorIOConfig.sample_run_config(f"distributions/{name}"),
     )
 
     core_model_to_hf_repo = {m.descriptor(): m.huggingface_repo for m in all_registered_models()}
@@ -71,14 +68,6 @@ def get_distribution_template() -> DistributionTemplate:
         )
         for m in MODEL_ENTRIES
     ]
-    embedding_model = ModelInput(
-        model_id="all-MiniLM-L6-v2",
-        provider_id="sentence-transformers",
-        model_type=ModelType.embedding,
-        metadata={
-            "embedding_dimension": 384,
-        },
-    )
     default_tool_groups = [
         ToolGroupInput(
             toolgroup_id="builtin::websearch",
@@ -93,13 +82,21 @@ def get_distribution_template() -> DistributionTemplate:
             provider_id="code-interpreter",
         ),
     ]
+    embedding_model = ModelInput(
+        model_id="all-MiniLM-L6-v2",
+        provider_id="sentence-transformers",
+        model_type=ModelType.embedding,
+        metadata={
+            "embedding_dimension": 384,
+        },
+    )
 
     return DistributionTemplate(
         name=name,
         distro_type="self_hosted",
-        description="Use Fireworks.AI for running LLM inference",
+        description="Distribution for running e2e tests in CI",
         container_image=None,
-        template_path=Path(__file__).parent / "doc_template.md",
+        template_path=None,
         providers=providers,
         default_models=default_models,
         run_configs={
@@ -109,53 +106,8 @@ def get_distribution_template() -> DistributionTemplate:
                     "vector_io": [vector_io_provider],
                 },
                 default_models=default_models + [embedding_model],
+                default_tool_groups=default_tool_groups,
                 default_shields=[ShieldInput(shield_id="meta-llama/Llama-Guard-3-8B")],
-                default_tool_groups=default_tool_groups,
-            ),
-            "run-with-safety.yaml": RunConfigSettings(
-                provider_overrides={
-                    "inference": [
-                        inference_provider,
-                        embedding_provider,
-                    ],
-                    "vector_io": [vector_io_provider],
-                    "safety": [
-                        Provider(
-                            provider_id="llama-guard",
-                            provider_type="inline::llama-guard",
-                            config={},
-                        ),
-                        Provider(
-                            provider_id="llama-guard-vision",
-                            provider_type="inline::llama-guard",
-                            config={},
-                        ),
-                        Provider(
-                            provider_id="code-scanner",
-                            provider_type="inline::code-scanner",
-                            config={},
-                        ),
-                    ],
-                },
-                default_models=[
-                    *default_models,
-                    embedding_model,
-                ],
-                default_shields=[
-                    ShieldInput(
-                        shield_id="meta-llama/Llama-Guard-3-8B",
-                        provider_id="llama-guard",
-                    ),
-                    ShieldInput(
-                        shield_id="meta-llama/Llama-Guard-3-11B-Vision",
-                        provider_id="llama-guard-vision",
-                    ),
-                    ShieldInput(
-                        shield_id="CodeScanner",
-                        provider_id="code-scanner",
-                    ),
-                ],
-                default_tool_groups=default_tool_groups,
             ),
         },
         run_config_env_vars={
@@ -165,7 +117,7 @@ def get_distribution_template() -> DistributionTemplate:
             ),
             "FIREWORKS_API_KEY": (
                 "",
-                "Fireworks.AI API Key",
+                "Fireworks API Key",
             ),
         },
     )
