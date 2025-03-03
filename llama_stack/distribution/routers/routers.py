5f3ec93a35de26874bac41562391ef2f9fbfe6c2
Eric Huang <erichuang@meta.com>
2025-03-03 12:09:07 -0800

    feat: better using get_default_tool_prompt_format
    Summary:
    
    Test Plan:

--- llama_stack/distribution/routers/routers.py
+++ llama_stack/distribution/routers/routers.py
@@ -4,7 +4,6 @@
 # This source code is licensed under the terms described in the LICENSE file in
 # the root directory of this source tree.
 
-import copy
 from typing import Any, AsyncGenerator, Dict, List, Optional
 
 from llama_stack import logcat
@@ -54,7 +53,6 @@ from llama_stack.apis.tools import (
 )
 from llama_stack.apis.vector_io import Chunk, QueryChunksResponse, VectorIO
 from llama_stack.providers.datatypes import RoutingTable
-from llama_stack.providers.utils.inference.prompt_adapter import get_default_tool_prompt_format
 
 
 class VectorIORouter(VectorIO):
@@ -180,9 +178,6 @@ class InferenceRouter(Inference):
             if tool_prompt_format:
                 params["tool_prompt_format"] = tool_prompt_format
             tool_config = ToolConfig(**params)
-
-        tool_config = copy.copy(tool_config)
-        tool_config.tool_prompt_format = tool_config.tool_prompt_format or get_default_tool_prompt_format(model_id)
 
         tools = tools or []
         if tool_config.tool_choice == ToolChoice.none:
