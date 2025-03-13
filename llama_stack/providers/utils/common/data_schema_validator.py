0c07811f8e6b64eea4198a17870f9322f0aa4009
Yang Yang <yyy@meta.com>
2025-03-12 17:18:01 -0700

    new cols in the explicit schema too

--- llama_stack/providers/utils/common/data_schema_validator.py
+++ llama_stack/providers/utils/common/data_schema_validator.py
@@ -41,6 +41,15 @@ VALID_SCHEMAS_FOR_SCORING = [
         ColumnName.generated_answer.value: StringType(),
         ColumnName.context.value: StringType(),
     },
+    {
+        ColumnName.input_query.value: StringType(),
+        ColumnName.expected_answer.value: StringType(),
+        ColumnName.generated_answer.value: StringType(),
+        ColumnName.function.value: StringType(),
+        ColumnName.language.value: StringType(),
+        ColumnName.id.value: StringType(),
+        ColumnName.ground_truth.value: StringType(),
+    },
 ]
 
 VALID_SCHEMAS_FOR_EVAL = [
@@ -53,6 +62,15 @@ VALID_SCHEMAS_FOR_EVAL = [
         ColumnName.input_query.value: StringType(),
         ColumnName.expected_answer.value: StringType(),
         ColumnName.completion_input.value: CompletionInputType(),
+    },
+    {
+        ColumnName.input_query.value: StringType(),
+        ColumnName.expected_answer.value: StringType(),
+        ColumnName.generated_answer.value: StringType(),
+        ColumnName.function.value: StringType(),
+        ColumnName.language.value: StringType(),
+        ColumnName.id.value: StringType(),
+        ColumnName.ground_truth.value: StringType(),
     },
 ]
 
