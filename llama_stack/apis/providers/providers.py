a062723d031fa715dd2f1e98ef299e1845ad0a41
Charlie Doern <cdoern@redhat.com>
2025-03-13 18:07:21 -0400

    feat: add provider API for listing and inspecting provider info (#1429)
    # What does this PR do?
    
    currently the `inspect` API for providers is really a `list` API. Create
    a new `providers` API which has a GET `providers/{provider_id}` inspect
    API
    which returns "user friendly" configuration to the end user. Also add a
    GET `/providers` endpoint which returns the list of providers as
    `inspect/providers` does today.
    
    This API follows CRUD and is more intuitive/RESTful.
    
    This work is part of the RFC at
    https://github.com/meta-llama/llama-stack/pull/1359
    
    sensitive fields are redacted using `redact_sensetive_fields` on the
    server side before returning a response:
    
    <img width="456" alt="Screenshot 2025-03-13 at 4 40 21 PM"
    src="https://github.com/user-attachments/assets/9465c221-2a26-42f8-a08a-6ac4a9fecce8"
    />
    
    
    ## Test Plan
    
    using https://github.com/meta-llama/llama-stack-client-python/pull/181 a
    user is able to to run the following:
    
    `llama stack build --template ollama --image-type venv`
    `llama stack run --image-type venv
    ~/.llama/distributions/ollama/ollama-run.yaml`
    `llama-stack-client providers inspect ollama`
    
    <img width="378" alt="Screenshot 2025-03-13 at 4 39 35 PM"
    src="https://github.com/user-attachments/assets/8273d05d-8bc3-44c6-9e4b-ef95e48d5466"
    />
    
    
    also, was able to run the new test_list integration test locally with
    ollama:
    
    <img width="1509" alt="Screenshot 2025-03-13 at 11 03 40 AM"
    src="https://github.com/user-attachments/assets/9b9db166-f02f-45b0-86a4-306d85149bc8"
    />
    
    Signed-off-by: Charlie Doern <cdoern@redhat.com>

---
+++ llama_stack/apis/providers/providers.py
@@ -0,0 +1,40 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the terms described in the LICENSE file in
+# the root directory of this source tree.
+
+from typing import List, Protocol, runtime_checkable
+
+from pydantic import BaseModel
+
+from llama_stack.distribution.datatypes import Provider
+from llama_stack.schema_utils import json_schema_type, webmethod
+
+
+@json_schema_type
+class ProviderInfo(BaseModel):
+    api: str
+    provider_id: str
+    provider_type: str
+
+
+class GetProviderResponse(BaseModel):
+    data: Provider | None
+
+
+class ListProvidersResponse(BaseModel):
+    data: List[ProviderInfo]
+
+
+@runtime_checkable
+class Providers(Protocol):
+    """
+    Providers API for inspecting, listing, and modifying providers and their configurations.
+    """
+
+    @webmethod(route="/providers", method="GET")
+    async def list_providers(self) -> ListProvidersResponse: ...
+
+    @webmethod(route="/providers/{provider_id}", method="GET")
+    async def inspect_provider(self, provider_id: str) -> GetProviderResponse: ...
