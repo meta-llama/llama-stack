59ff6ead2cd9f016a3f1360d23b8e03a19d704c8
Xi Yan <xiyan@meta.com>
2025-03-13 16:37:07 -0700

    remove json

--- llama_stack/apis/datasets/datasets.py
+++ llama_stack/apis/datasets/datasets.py
@@ -13,7 +13,6 @@ from llama_stack.apis.resource import Resource, ResourceType
 from llama_stack.schema_utils import json_schema_type, register_schema, webmethod
 
 
-@json_schema_type
 class DatasetPurpose(str, Enum):
     """
     Purpose of the dataset. Each purpose has a required input data schema.
