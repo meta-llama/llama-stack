c82ae6e7daa8d6e777ed67af84a4c9f8113ce495
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2025-03-19 13:09:31 -0400

    fix: Use CONDA_DEFAULT_ENV presence as a flag to use conda mode
    Note: Conda will only be used when --image-name is unset *and*
    CONDA_DEFAULT_ENV is set. This means that users without conda will
    correctly fall back to using system packages when no --image-* arguments
    are passed at all.
    
    Signed-off-by: Ihar Hrachyshka <ihar.hrachyshka@gmail.com>

--- llama_stack/cli/stack/utils.py
+++ llama_stack/cli/stack/utils.py
@@ -4,6 +4,14 @@
 # This source code is licensed under the terms described in the LICENSE file in
 # the root directory of this source tree.
 
+from enum import Enum
+
+
+class ImageType(Enum):
+    CONDA = "conda"
+    CONTAINER = "container"
+    VENV = "venv"
+
 
 def print_subcommand_description(parser, subparsers):
     """Print descriptions of subcommands."""
