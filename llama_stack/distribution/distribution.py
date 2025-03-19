d1b44c1251d387d06477abc6f9d327f0d7f9ea26
Xi Yan <xiyan@meta.com>
2025-03-19 09:55:18 -0700

    benchmark->evaluation routing?

--- llama_stack/distribution/distribution.py
+++ llama_stack/distribution/distribution.py
@@ -43,13 +43,21 @@ def builtin_automatically_routed_apis() -> List[AutoRoutedApiInfo]:
             routing_table_api=Api.tool_groups,
             router_api=Api.tool_runtime,
         ),
+        AutoRoutedApiInfo(
+            routing_table_api=Api.benchmarks,
+            router_api=Api.evaluation,
+        ),
     ]
 
 
 def providable_apis() -> List[Api]:
-    routing_table_apis = {x.routing_table_api for x in builtin_automatically_routed_apis()}
+    routing_table_apis = {
+        x.routing_table_api for x in builtin_automatically_routed_apis()
+    }
     return [
-        api for api in Api if api not in routing_table_apis and api not in [Api.inspect, Api.providers, Api.benchmarks]
+        api
+        for api in Api
+        if api not in routing_table_apis and api not in [Api.inspect, Api.providers]
     ]
 
 
