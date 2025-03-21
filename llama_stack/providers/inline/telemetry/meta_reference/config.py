34f89bfbd6020970a64ce90fa64282103a88f1cc
ehhuang <ehhuang@users.noreply.github.com>
2025-03-21 12:02:10 -0700

    feat(telemetry): use zero-width space to avoid clutter (#1754)
    # What does this PR do?
    Before 
    <img width="858" alt="image"
    src="https://github.com/user-attachments/assets/6cefb1ae-5603-4818-85ea-a0c337b986bc"
    />
    
    Note the redundant 'llama-stack' in front of every span
    
    ## Test Plan
    <img width="1171" alt="image"
    src="https://github.com/user-attachments/assets/bdc5fd5b-ff1f-4f10-8b40-cff2ea93dd1f"
    />

--- llama_stack/providers/inline/telemetry/meta_reference/config.py
+++ llama_stack/providers/inline/telemetry/meta_reference/config.py
@@ -29,7 +29,8 @@ class TelemetryConfig(BaseModel):
         description="The OpenTelemetry collector endpoint URL for metrics",
     )
     service_name: str = Field(
-        default="llama-stack",
+        # service name is always the same, use zero-width space to avoid clutter
+        default="​",
         description="The service name to use for telemetry",
     )
     sinks: List[TelemetrySink] = Field(
@@ -51,7 +52,7 @@ class TelemetryConfig(BaseModel):
     @classmethod
     def sample_run_config(cls, __distro_dir__: str, db_name: str = "trace_store.db") -> Dict[str, Any]:
         return {
-            "service_name": "${env.OTEL_SERVICE_NAME:llama-stack}",
+            "service_name": "${env.OTEL_SERVICE_NAME:​}",
             "sinks": "${env.TELEMETRY_SINKS:console,sqlite}",
             "sqlite_db_path": "${env.SQLITE_DB_PATH:" + __distro_dir__ + "/" + db_name + "}",
         }
