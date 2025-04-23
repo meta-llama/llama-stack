2c682b086a829dcab2206aae78f9ddead42eb5fc
Divya <divyaruhil999@gmail.com>
2025-04-23 04:09:49 -0700

    Adding TLS support one-way

--- llama_stack/providers/remote/vector_io/milvus/config.py
+++ llama_stack/providers/remote/vector_io/milvus/config.py
@@ -16,7 +16,12 @@ class MilvusVectorIOConfig(BaseModel):
     uri: str
     token: str | None = None
     consistency_level: str = "Strong"
-
+    # TLS-related options
+    secure: bool = False
+    server_pem_path: Optional[str] = None
     @classmethod
-    def sample_run_config(cls, __distro_dir__: str, **kwargs: Any) -> dict[str, Any]:
-        return {"uri": "${env.MILVUS_ENDPOINT}", "token": "${env.MILVUS_TOKEN}"}
+
+    def sample_run_config(cls, __distro_dir__: str, **kwargs: Any) -> Dict[str, Any]:
+        return {"uri": "${env.MILVUS_ENDPOINT}", "token": "${env.MILVUS_TOKEN}","secure": True,
+            "server_pem_path": "/path/to/ca.crt"}
+
