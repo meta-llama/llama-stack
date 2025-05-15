1a6d4af5e9217a253607d263be4186167d40d215
Ashwin Bharambe <ashwin.bharambe@gmail.com>
2025-05-15 12:52:34 -0700

    refactor: rename dev distro as starter (#2181)
    We want this to be a "flagship" distribution we can advertize to a
    segment of users to get started quickly. This distro should package a
    bunch of remote providers and some cheap inline providers so they get a
    solid "AI Platform in a box" setup instantly.

--- llama_stack/templates/starter/__init__.py
+++ llama_stack/templates/starter/__init__.py
@@ -4,4 +4,4 @@
 # This source code is licensed under the terms described in the LICENSE file in
 # the root directory of this source tree.
 
-from .dev import get_distribution_template  # noqa: F401
+from .starter import get_distribution_template  # noqa: F401
