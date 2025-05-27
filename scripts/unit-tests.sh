49f5d33ec7b24113bd5ca0adc1feca069a037726
Sébastien Han <seb@redhat.com>
2025-05-27 22:00:59 +0200

    chore: use dependency-groups for dev
    The previous `[project.optional-dependencies]` was misrepresenting what
    the packages were. They were NOT optional dependencies to the project
    but development dependencies. Unlike optional dependencies, development
    dependencies are local-only and will not be included in the project
    requirements when published to PyPI or other indexes. As such,
    development dependencies are not included in the [project]
    table.  Additionally, the dev group is synced by default.
    
    Source:
    https://docs.astral.sh/uv/concepts/projects/dependencies/#development-dependencies
    
    Signed-off-by: Sébastien Han <seb@redhat.com>

--- scripts/unit-tests.sh
+++ scripts/unit-tests.sh
@@ -10,10 +10,10 @@ PYTHON_VERSION=${PYTHON_VERSION:-3.10}
 
 command -v uv >/dev/null 2>&1 || { echo >&2 "uv is required but it's not installed. Exiting."; exit 1; }
 
-uv python find $PYTHON_VERSION
+uv python find "$PYTHON_VERSION"
 FOUND_PYTHON=$?
 if [ $FOUND_PYTHON -ne 0 ]; then
-     uv python install $PYTHON_VERSION
+     uv python install "$PYTHON_VERSION"
 fi
 
-uv run --python $PYTHON_VERSION --with-editable . --with-editable ".[dev]" --with-editable ".[unit]" pytest --asyncio-mode=auto -s -v tests/unit/ $@
+uv run --python "$PYTHON_VERSION" --with-editable . --group unit pytest --asyncio-mode=auto -s -v tests/unit/ $@
