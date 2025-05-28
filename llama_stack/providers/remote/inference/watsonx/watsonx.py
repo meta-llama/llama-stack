bc58bbf94c24e9297fbe8deae6aac07abe8b6c65
Sumit Jaiswal <sjaiswal@redhat.com>
2025-05-29 02:16:29 +0530

    remove unused asyncio lib

--- llama_stack/providers/remote/inference/watsonx/watsonx.py
+++ llama_stack/providers/remote/inference/watsonx/watsonx.py
@@ -4,7 +4,6 @@
 # This source code is licensed under the terms described in the LICENSE file in
 # the root directory of this source tree.
 
-import asyncio
 from collections.abc import AsyncGenerator, AsyncIterator
 from typing import Any
 
