35c2817d0ae94ab8eda837a1f1b4eef0f9a6ae60
Ibrahim Haroon <99413953+Ibrahim-Haroon@users.noreply.github.com>
2025-06-12 11:23:59 -0400

    fix(weaviate): handle case where distance is 0 by setting score to infinity (#2415)
    # What does this PR do?
    Fixes provider weaviate `query_vector` function for when the distance
    between the query embedding and an embedding within the vector db is 0
    (identical vectors). Catches `ZeroDivisionError` and then sets `score`
    to infinity, which represent maximum similarity.
    
    <!-- If resolving an issue, uncomment and update the line below -->
    Closes [#2381]
    
    ## Test Plan
    Checkout this PR
    
    Execute this code and there will no longer be a `ZeroDivisionError`
    exception
    ```
    from llama_stack_client import LlamaStackClient
    
    base_url = "http://localhost:8321"
    client = LlamaStackClient(base_url=base_url)
    
    models = client.models.list()
    embedding_model = (
        em := next(m for m in models if m.model_type == "embedding")
    ).identifier
    embedding_dimension = 384
    
    _ = client.vector_dbs.register(
        vector_db_id="foo_db",
        embedding_model=embedding_model,
        embedding_dimension=embedding_dimension,
        provider_id="weaviate",
    )
    
    chunk = {
        "content": "foo",
        "mime_type": "text/plain",
        "metadata": {
            "document_id": "foo-id"
        }
    }
    
    client.vector_io.insert(vector_db_id="foo_db", chunks=[chunk])
    client.vector_io.query(vector_db_id="foo_db", query="foo")
    ```

--- llama_stack/providers/remote/vector_io/weaviate/weaviate.py
+++ llama_stack/providers/remote/vector_io/weaviate/weaviate.py
@@ -76,7 +76,7 @@ class WeaviateIndex(EmbeddingIndex):
                 continue
 
             chunks.append(chunk)
-            scores.append(1.0 / doc.metadata.distance)
+            scores.append(1.0 / doc.metadata.distance if doc.metadata.distance != 0 else float("inf"))
 
         return QueryChunksResponse(chunks=chunks, scores=scores)
 
