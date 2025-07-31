d6e0ee979c8edb42832f235c649d0c116e563399
Matthew Farrellee <matt@cs.wisc.edu>
2025-07-31 15:41:29 -0500

    remove SambaNovaInferenceAdapter's custom register_model in favor of the mixin's behavior

--- llama_stack/providers/remote/inference/sambanova/sambanova.py
+++ llama_stack/providers/remote/inference/sambanova/sambanova.py
@@ -4,9 +4,6 @@
 # This source code is licensed under the terms described in the LICENSE file in
 # the root directory of this source tree.
 
-import requests
-
-from llama_stack.apis.models import Model
 from llama_stack.log import get_logger
 from llama_stack.providers.utils.inference.litellm_openai_mixin import LiteLLMOpenAIMixin
 
@@ -32,22 +29,6 @@ class SambaNovaInferenceAdapter(LiteLLMOpenAIMixin):
             download_images=True,  # SambaNova requires base64 image encoding
             json_schema_strict=False,  # SambaNova doesn't support strict=True yet
         )
-
-    async def register_model(self, model: Model) -> Model:
-        model_id = self.get_provider_model_id(model.provider_resource_id)
-
-        list_models_url = self.config.url + "/models"
-        if len(self.environment_available_models) == 0:
-            try:
-                response = requests.get(list_models_url)
-                response.raise_for_status()
-            except requests.exceptions.RequestException as e:
-                raise RuntimeError(f"Request to {list_models_url} failed") from e
-            self.environment_available_models = [model.get("id") for model in response.json().get("data", {})]
-
-        if model_id.split("sambanova/")[-1] not in self.environment_available_models:
-            logger.warning(f"Model {model_id} not available in {list_models_url}")
-        return model
 
     async def initialize(self):
         await super().initialize()
