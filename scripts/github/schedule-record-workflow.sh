b04468ca781b3a58ca80f2e1019547591e9e28cb
Ashwin Bharambe <ashwin.bharambe@gmail.com>
2025-08-15 16:14:13 -0700

    fix

--- scripts/github/schedule-record-workflow.sh
+++ scripts/github/schedule-record-workflow.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the terms described in the LICENSE file in
+# the root directory of this source tree.
+
 # Script to easily trigger the integration test recording workflow
 # Usage: ./scripts/github/schedule-record-workflow.sh [options]
 
