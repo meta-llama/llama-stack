2e7ca0742357eddfc9b4738989aaebbd9bbde52b
Ashwin Bharambe <ashwin.bharambe@gmail.com>
2025-08-18 14:58:23 -0700

    feat(cli): make venv the default image type (#3187)
    We have removed conda now so we can make `venv` the default. Just doing
    `llama stack build --distro starter` is now enough for the most part.

--- llama_stack/cli/stack/build.py
+++ llama_stack/cli/stack/build.py
@@ -59,7 +59,7 @@ class StackBuild(Subcommand):
             type=str,
             help="Image Type to use for the build. If not specified, will use the image type from the template config.",
             choices=[e.value for e in ImageType],
-            default=None,  # no default so we can detect if a user specified --image-type and override image_type in the config
+            default=ImageType.VENV.value,
         )
 
         self.parser.add_argument(
